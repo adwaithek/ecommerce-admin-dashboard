cf4b8461d042136b226002ae5cf9331abba7d077
adwaithek <adwaithek01@gmail.com>
2023-04-08 16:17:37 +0530

    fin

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import { Ecommerce, Orders, Employees, Customers, Line, Pie, Financial, } from './pages';
+import './App.css';
+
+import { useStateContext } from './contexts/ContextProvider';
+
+const App = () => {
+  const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings } = useStateContext();
+
+  useEffect(() => {
+    const currentThemeColor = localStorage.getItem('colorMode');
+    const currentThemeMode = localStorage.getItem('themeMode');
+    if (currentThemeColor && currentThemeMode) {
+      setCurrentColor(currentThemeColor);
+      setCurrentMode(currentThemeMode);
+    }
+  }, []);
+
+  return (
+    <div className={currentMode === 'Dark' ? 'dark' : ''}>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+        
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+              <Sidebar />
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>
+          )}
+          <div
+            className={
+              activeMenu
+                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+            }
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+              <Navbar />
+            </div>
+            <div>
+              {themeSettings && (<ThemeSettings />)}
+
+              <Routes>
+                {/* dashboard  */}
+                <Route path="/" element={(<Ecommerce />)} />
+                <Route path="/ecommerce" element={(<Ecommerce />)} />
+
+                {/* pages  */}
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/employees" element={<Employees />} />
+                <Route path="/customers" element={<Customers />} />
+
+
+
+                {/* charts  */}
+                <Route path="/line" element={<Line />} />
+
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/financial" element={<Financial />} />
+
+
+              </Routes>
+            </div>
+            <Footer />
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+export { default as Ecommerce } from './Ecommerce';
+
+export { default as Orders } from './Orders';
+export { default as Employees } from './Employees';
+export { default as Customers } from './Customers';
+
+
+
+export { default as Financial } from './Charts/Financial';
+export { default as Line } from './Charts/Line';
+export { default as Pie } from './Charts/Pie';
+
+
+
